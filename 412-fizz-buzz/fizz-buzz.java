87ad4d4019df4f337865de003ce16d3e7b27a567
rohitsingh88579-max <rohitsingh88579@gmail.com>
2026-08-03 22:44:25 +0530

    Time: 1 ms (99.90%) | Memory: 46.6 MB (93.24%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+    public List<String> fizzBuzz(int n) 
+    {
+        List<String> str=new ArrayList<>();
+        for(int x=1;x<=n;x++)
+        {
+            if(x%3==0 && x%5==0)
+            {
+                 str.add("FizzBuzz");
+            }
+            else if(x%5==0)
+            {
+                 str.add("Buzz");
+            }
+            else if(x%3==0 )
+            {
+                 str.add("Fizz");
+            }
+            else
+            {
+                str.add(Integer.toString(x));
+            }
+        }
+        return str;
+    }
+}
