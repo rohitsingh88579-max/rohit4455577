af579981fe84b29ac50fdefba40424524723d6de
rohitsingh88579-max <rohitsingh88579@gmail.com>
2026-08-05 18:27:12 +0530

    Time: 1 ms (99.96%) | Memory: 71.2 MB (60.33%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,35 +1,37 @@
-class Solution             
+class Solution 
 {
-    public int firstMissingPositive(int[] nums)                  
+    public int firstMissingPositive(int[] nums) 
     {
-         int n=nums.length;
-         for(int x=0;x<n;x++)
-         {
+        int  n=nums.length;
+        //Cleaning da Array
+        for(int x=0;x<n;x++)
+        {
             if(nums[x]>n||nums[x]<=0)
             {
                 nums[x]=n+1;
             }
-         }
-         for(int x=0;x<n;x++)
-         {
-            int num=Math.abs(nums[x]);  
+        }    
+        //Making ts easy and negative
+        for(int x=0;x<n;x++)
+        {
+            int num=Math.abs(nums[x]);
             if(num>n)
-            {
+             {
                 continue;
-            }       
-            int index=num-1;
-            if(nums[index]>0)
-            {
+             }
+             int index=num-1;
+             if(nums[index]>0)
+             {
                 nums[index]=-nums[index];
-            }   
-         }
-         for(int x=0;x<n;x++)
-         {
+             }
+        }
+        for(int x=0;x<n;x++)
+        {
             if(nums[x]>0)
             {
                 return x+1;
             }
-         }
-         return n+1;
+        }
+        return n+1;
     }
 }
