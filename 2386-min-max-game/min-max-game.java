75cbd53a533ade0d64f9e9d1bb8658a802f2c932
rohitsingh88579-max <rohitsingh88579@gmail.com>
2026-08-24 14:03:31 +0530

    Time: 1 ms (73.42%) | Memory: 44.9 MB (86.38%) - LeetSync

---
+++ 2386-min-max-game/min-max-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minMaxGame(int[] nums) {
+        int n = nums.length;
+        
+        while (n > 1) {
+            n /= 2;
+            for (int i = 0; i < n; i++) {
+                if (i % 2 == 0) {
+                    nums[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+                } else {
+                    nums[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+                }
+            }
+        }
+        
+        return nums[0];
+    }
+}
