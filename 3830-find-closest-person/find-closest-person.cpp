892783204813d44bff9098ed2e324c2e8abda592
rohitsingh88579-max <rohitsingh88579@gmail.com>
2026-08-31 20:04:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.07%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public:
+    int findClosest(int x, int y, int z) 
+    {
+       int d1=abs(z-x);
+       int d2=abs(z-y);
+       if(d1==d2)
+       {
+        return 0;
+       }    
+       if(d1>d2)
+       {
+        return 2;
+       }
+       return 1;
+    }
+};
