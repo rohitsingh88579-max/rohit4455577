53d17eb653c3cc7ceb4d851fffb87293d6400a28
rohitsingh88579-max <rohitsingh88579@gmail.com>
2026-09-02 22:42:08 +0530

    Time: 3 ms (5.87%) | Memory: 31.4 MB (7.23%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,6 +1,24 @@
-class Solution {
-public:
-    bool uniformArray(vector<int>& nums1) {
-        return true;
+class Solution 
+{
+    public:
+    bool uniformArray(vector<int>& nums1) 
+    {
+       int n=nums1.size();
+       vector<int>nums2;
+       vector<int>nums3;
+       for(int x=0;x<n;x++)    
+       {
+          if(nums1[x]%2==0)
+          {
+             nums2=nums1;
+             return true;
+          }
+          else if(nums1[x]%2!=0)
+          {
+            nums3=nums1;
+            return true;
+          }
+       }
+       return false;
     }
 };
