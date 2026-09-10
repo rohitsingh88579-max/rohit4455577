9986c538b480223da8481d979a5c309ee0477711
rohitsingh88579-max <rohitsingh88579@gmail.com>
2026-09-10 19:26:30 +0530

    Time: 7 ms (96.76%) | Memory: 47.2 MB (28.86%) - LeetSync

--- 1260-day-of-the-year/day-of-the-year.java
+++ 1260-day-of-the-year/day-of-the-year.java
@@ -2,37 +2,35 @@ class Solution
 {
     public int dayOfYear(String date) 
     {
-        int ar[]={31,28,31,30,31,30,31,31,30,31,30,31};
-        int year=Integer.parseInt(date.substring(0,4));
-        int month=Integer.parseInt(date.substring(5,7));
-        int day=Integer.parseInt(date.substring(8,10));
-        int num=0;
-        if(leap(year))
-        {
-            ar[1]=29;
-        }
-        for(int x=0;x<month-1;x++)
-        {
-            num+=ar[x];
-        }
-        num+=day;
-        return num;
+       int ar[]={31,28,31,30,31,30,31,31,30,31,30,31};
+       int year=Integer.parseInt(date.substring(0,4));
+       int month=Integer.parseInt(date.substring(5,7));
+       int day=Integer.parseInt(date.substring(8));
+       int num=0;
+       if(leap(year))
+       {
+         ar[1]=29;
+       }    
+       for(int x=0;x<month-1;x++)
+       {
+         num+=ar[x];
+       }
+       return num+day;
     }
-    public boolean leap(int n)
+    public boolean leap(int x)
     {
-        if(n%400==0 )
+        if(x%400==0)
         {
             return true;
         }
-        if(n%100==0)
+        if(x%100==0)
         {
             return false;
         }
-        if(n%4==0)
+        if(x%4==0)
         {
             return true;
         }
         return false;
-
     }
 }
