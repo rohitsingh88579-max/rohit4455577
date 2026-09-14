9d2ec5a4090c16bec36d11901e79c7f4e9c84622
rohitsingh88579-max <rohitsingh88579@gmail.com>
2026-09-14 18:11:57 +0530

    Time: 2 ms (16.18%) | Memory: 20.4 MB (82.99%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -5,10 +5,10 @@ class Solution
     {
         int n=nums.size();
         vector<int>temp(nums.size());
-        for(int x=0;x<n;x++)    
+        for(int x=0;x<n;x++)
         {
             temp[x]=nums[nums[x]];
-        }
+        }    
         return temp;
     }
 };
