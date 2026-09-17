af9f8036b7693837731f9e619656815ae2156158
rohitsingh88579-max <rohitsingh88579@gmail.com>
2026-09-17 20:03:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (64.64%) - LeetSync

---
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public:
+    vector<int> separateDigits(vector<int>& nums) 
+    {
+       vector<int> ans;
+       for(int x:nums)
+       {
+          string s=to_string(x);
+          for(char c:s)
+          {
+            ans.push_back(c-'0');
+          }
+       }    
+       return ans;
+    }
+};
