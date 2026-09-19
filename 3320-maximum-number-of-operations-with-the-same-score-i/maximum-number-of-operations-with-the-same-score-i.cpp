6f54607965fd1897e0e1f2b04da8a61f87f7f4ba
rohitsingh88579-max <rohitsingh88579@gmail.com>
2026-09-19 18:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (65.97%) - LeetSync

--- 3320-maximum-number-of-operations-with-the-same-score-i/maximum-number-of-operations-with-the-same-score-i.cpp
+++ 3320-maximum-number-of-operations-with-the-same-score-i/maximum-number-of-operations-with-the-same-score-i.cpp
@@ -1,23 +1,23 @@
 class Solution 
 {
     public:
-    int maxOperations(vector<int>& nums) 
+    int maxOperations(vector<int>& nums)
     {
-        int sum=nums[0]+nums[1];
-        int count=0;
-        int x=2;
-        while(x+1<nums.size())
+       int sum=nums[0]+nums[1];
+       int x=2;
+       int count=1;
+       while(x+1<nums.size())
+       {
+         if(nums[x]+nums[x+1]==sum)
         {
-            if(nums[x]+nums[x+1]==sum)
-            {
-                count++;
-                x+=2;
-            }
-            else
-            {
-                break;
-            }
-        }  
-        return count+1;
+            count++;
+            x+=2;
+        }
+        else
+        {
+            break;
+        }
+       }    
+       return count;
     }
 };
