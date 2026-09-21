ca95b63a5eda10f7c082e1b6c3264f89025f1720
rohitsingh88579-max <rohitsingh88579@gmail.com>
2026-09-21 18:12:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (72.17%) - LeetSync

--- 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.java
@@ -2,8 +2,8 @@ class Solution
 {
     public boolean checkTwoChessboards(String coordinate1, String coordinate2) 
     {
-        int m=coordinate1.charAt(0)-'a'+coordinate1.charAt(1)-'a';
-        int n=coordinate2.charAt(0)-'a'+coordinate2.charAt(1)-'a';
+        int m=coordinate1.charAt(0)-'a'+coordinate1.charAt(1)-'1';
+        int n=coordinate2.charAt(0)-'a'+coordinate2.charAt(1)-'1';
         if(m%2==n%2)
         {
             return true;
